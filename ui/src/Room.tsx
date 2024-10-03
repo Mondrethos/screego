832809a5e361cd63f170f0e6795ceefe59d55fda
Max <max@maximo.me>
2024-10-03 18:36:19 -0500

    Update Room.tsx
    Audio Support

--- ui/src/Room.tsx
+++ ui/src/Room.tsx
@@ -3,6 +3,8 @@ import {Badge, IconButton, Paper, Theme, Tooltip, Typography} from '@mui/materia
 import CancelPresentationIcon from '@mui/icons-material/CancelPresentation';
 import PresentToAllIcon from '@mui/icons-material/PresentToAll';
 import FullScreenIcon from '@mui/icons-material/Fullscreen';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import VolumeMuteIcon from '@material-ui/icons/VolumeMute';
 import PeopleIcon from '@mui/icons-material/People';
 import SettingsIcon from '@mui/icons-material/Settings';
 import {useHotkeys} from 'react-hotkeys-hook';
@@ -248,6 +250,22 @@ export const Room = ({
                         <Badge badgeContent={state.users.length} color="primary">
                             <PeopleIcon fontSize="large" />
                         </Badge>
+                    </Tooltip>
+                    <Tooltip title="Sound" arrow>
+                        <IconButton
+                            onClick={() => {
+                                const video = videoElement as HTMLMediaElement;
+                                if (video) {
+                                    video.muted = !video.muted;
+                                }
+                            }}
+                            disabled={!selectedStream || !!state.hostStream}>
+                            {videoElement?.muted ? (
+                                <VolumeMuteIcon fontSize="large" />
+                            ) : (
+                                <VolumeUpIcon fontSize="large" />
+                            )}
+                        </IconButton>
                     </Tooltip>
                     <Tooltip title="Fullscreen" arrow>
                         <IconButton
